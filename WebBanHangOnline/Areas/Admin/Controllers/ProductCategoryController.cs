ee8adf9951c6de2d09a8d10bd5bcce6f738396a8
Huy Đức <huytrab234@gmail.com>
2024-10-18 18:16:56 +0700

    fix lai 1 số controller

--- WebBanHangOnline/Areas/Admin/Controllers/ProductCategoryController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,36 +8,56 @@ using WebBanHangOnline.Models.EF;
 
 namespace WebBanHangOnline.Areas.Admin.Controllers
 {
-    public class ProductImageController : Controller
+    public class ProductCategoryController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-        // GET: Admin/ProductImage
-        public ActionResult Index(int id)
+        // GET: Admin/ProductCategory
+        public ActionResult Index()
         {
-            ViewBag.ProductId = id;
-            var items = db.ProductImages.Where(x => x.ProductId == id).ToList();
+            var items = db.ProductCategories;
             return View(items);
         }
 
-        [HttpPost]
-        public ActionResult AddImage(int productId, string url)
+        public ActionResult Add()
         {
-            db.ProductImages.Add(new ProductImage
-            {
-                ProductId = productId,
-                Image = url,
-                IsDefault = false
-            });
-            db.SaveChanges();
-            return Json(new { Success = true });
+            return View();
         }
+
         [HttpPost]
-        public ActionResult Delete(int id)
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(ProductCategory model) //Kiểm tra xem dữ liệu đầu vào từ phía người dùng (trong form nhập liệu) có hợp lệ hay không,
         {
-            var item = db.ProductImages.Find(id);
-            db.ProductImages.Remove(item);
-            db.SaveChanges();
-            return Json(new { success = true });
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now; //Gán ngày và giờ hiện tại cho thuộc tín
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title); //loại bỏ các ký tự không hợp lệ hoặc thay thế dấu cách bằng dấu gạch ngang
+                db.ProductCategories.Add(model);  //Thêm đối tượng ProductCategory vào bảng ProductCategories trong cơ sở dữ liệu.
+                db.SaveChanges(); //Lưu những thay đổi vào cơ sở dữ liệu 
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var item = db.ProductCategories.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProductCategory model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.ProductCategories.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
--- WebBanHangOnline/Areas/Admin/Controllers/RoleController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/RoleController.cs
@@ -1,16 +1,23 @@
-﻿using WebBanHangOnline.Models;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
 
 namespace WebBanHangOnline.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
-    public class RoleController : Controller
+    public class RoleController : Controller //Controller này chịu trách nhiệm xử lý các yêu cầu từ người dùng và trả về các kết quả 
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationDbContext db = new ApplicationDbContext(); //Đây là đối tượng cho phép tương tác với cơ sở dữ liệu.
         // GET: Admin/Role
-        public ActionResult Index()
+        public ActionResult Index() //Index: Phương thức này trả về danh sách tất cả các vai trò (roles) hiện có trong cơ sở dữ liệu.
         {
-            var items = db.Roles.ToList();
-            return View(items);
+            var items = db.Roles.ToList(); //db.Roles.ToList(): Sử dụng đối tượng db để truy vấn danh sách các vai trò từ bảng Roles trong cơ sở dữ liệu và chuyển đổi chúng thành danh sách.
+            return View(items); //Kết quả được truyền vào view để hiển thị cho người dùng.
         }
 
 
@@ -23,10 +30,10 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(IdentityRole model)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) //Kiểm tra tính hợp lệ của dữ liệu do người dùng nhập vào. Nếu tất cả dữ liệu hợp lệ, mã sẽ tiếp tục chạy; nếu không hợp lệ, form sẽ được hiển thị lại với các thông báo lỗi.
             {
-                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
-                roleManager.Create(model);
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));//Sử dụng lớp RoleManager để quản lý các vai trò trong hệ thống. Một đối tượng mới được tạo ra để làm việc với các vai trò, sử dụng RoleStore để tương tác với cơ sở dữ liệu thông qua db.
+                roleManager.Create(model); //Phương thức này tạo một vai trò mới trong cơ sở dữ liệu với thuộc tính tù model
                 return RedirectToAction("Index");
             }
             return View(model);
@@ -41,9 +48,9 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(IdentityRole model)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid)//Kiểm tra tính hợp lệ của dữ liệu. Nếu hợp lệ, sẽ tiếp tục cập nhật vai trò.
             {
-                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));//Sử dụng phương thức Update của RoleManager để cập nhật thông tin vai trò trong cơ sở dữ liệu dựa trên dữ liệu từ model 
                 roleManager.Update(model);
                 return RedirectToAction("Index");
             }
