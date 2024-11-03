34304e1d9a6063c58b2ded8d01b347374b4feb4c
Huy Đức <huytrab234@gmail.com>
2024-11-03 22:46:04 +0700

    not

--- WebBanHangOnline/Areas/Admin/Controllers/StatisticalController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/StatisticalController.cs
@@ -1,4 +1,11 @@
-﻿using WebBanHangOnline.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
 
 namespace WebBanHangOnline.Areas.Admin.Controllers
 {
@@ -26,22 +33,23 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
                             Price = od.Price,
                             OriginalPrice = p.OriginalPrice
                         };
+            //kiểm tra xem fromDate và toDate có rỗng hay không nếu có giá trị, nó chuyển đổi chuỗi ngày sang định dạng DateTime
             if (!string.IsNullOrEmpty(fromDate))
             {
-                DateTime startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
+                DateTime startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null); //fromDate có giá trị, nó lọc các đơn hàng có CreatedDate lớn hơn hoặc bằng ngày bắt đầu.
                 query = query.Where(x => x.CreatedDate >= startDate);
             }
             if (!string.IsNullOrEmpty(toDate))
             {
-                DateTime endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
+                DateTime endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null); // toDate có giá trị, nó lọc các đơn hàng có CreatedDate nhỏ hơn ngày kết thúc.
                 query = query.Where(x => x.CreatedDate < endDate);
             }
 
-            var result = query.GroupBy(x => DbFunctions.TruncateTime(x.CreatedDate)).Select(x => new
+            var result = query.GroupBy(x => DbFunctions.TruncateTime(x.CreatedDate)).Select(x => new //DbFunctions.TruncateTime để nhóm dữ liệu theo ngày 
             {
                 Date = x.Key.Value,
-                TotalBuy = x.Sum(y => y.Quantity * y.OriginalPrice),
-                TotalSell = x.Sum(y => y.Quantity * y.Price),
+                TotalBuy = x.Sum(y => y.Quantity * y.OriginalPrice), //Tổng chi phí (TotalBuy): Tổng số lượng nhân với giá gốc (OriginalPrice).
+                TotalSell = x.Sum(y => y.Quantity * y.Price), //Tổng doanh thu (TotalSell): Tổng số lượng nhân với giá bán (Price).
             }).Select(x => new
             {
                 Date = x.Date,
